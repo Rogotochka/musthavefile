9bb8199a3b2af0951626d172d2b43141a710f1bc
Rogotochka <dimasokok216@gmail.com>
2026-05-27 10:21:26 +0300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,1 @@
+docker run --name="motioneye" -p 8765:8765 --hostname="motioneye" -v /etc/localtime:/etc/localtime:ro -v /etc/motioneye:/etc/motioneye -v /var/lib/motioneye:/var/lib/motioneye --device=/dev/video0 --device=/dev/video1 --device=/dev/video2 --restart="always" --detach=true ccrisan/motioneye:master-armhf
